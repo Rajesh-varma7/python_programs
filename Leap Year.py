2eafe83648dfff30bd99785ecfa8468563069044
Rajesh Varma Somu <99343209+Rajesh-varma7@users.noreply.github.com>
2025-05-08 12:23:55 +0530

    Create Leap Year.py

---
+++ Leap Year.py
@@ -0,0 +1,12 @@
+Leap_Year = int(input("Enter a Year")
+if Leap_Year %4 == 0:
+  if Leap_Year %100 == 0:
+    if Leap_Year %400 == 0:
+      print("Leap Year")
+    else:
+      print("Not Leap Year")
+  else:
+    primt("Leap Year")
+else:
+  print("Not Leap Year")
+  
